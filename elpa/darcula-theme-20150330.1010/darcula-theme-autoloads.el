681d4af5f49da2a12126d3881f25d693cb4d1e4c
an2nb2 <an2nb2@gmail.com>
2015-04-28 10:34:24 +0300

    init

---
+++ elpa/darcula-theme-20150330.1010/darcula-theme-autoloads.el
@@ -0,0 +1,26 @@
+;;; darcula-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "darcula-theme" "darcula-theme.el" (21815 42604
+;;;;;;  117695 79000))
+;;; Generated autoloads from darcula-theme.el
+
+(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil nil ("darcula-theme-pkg.el") (21815 42604 250393
+;;;;;;  615000))
+
+;;;***
+
+(provide 'darcula-theme-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; darcula-theme-autoloads.el ends here
