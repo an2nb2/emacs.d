e01deb7fbbc714a4063712dfbec097678284feb6
an2nb2 <an2nb2@gmail.com>
2015-07-10 17:24:08 +0300

    updates in keybindings

--- user-lisp/keybinds.el
+++ user-lisp/keybinds.el
@@ -1,10 +1,10 @@
 ;; Custom keybinds
 
 (defun my-keybindings (my-key-map)
-  (define-key my-key-map (kbd "<C-up>") 'shrink-window)
-  (define-key my-key-map (kbd "<C-down>") 'enlarge-window)
-  (define-key my-key-map (kbd "<C-left>") 'shrink-window-horizontally)
-  (define-key my-key-map (kbd "<C-right>") 'enlarge-window-horizontally)
+  (define-key my-key-map (kbd "<C-S-up>") 'shrink-window)
+  (define-key my-key-map (kbd "<C-S-down>") 'enlarge-window)
+  (define-key my-key-map (kbd "<C-S-left>") 'shrink-window-horizontally)
+  (define-key my-key-map (kbd "<C-S-right>") 'enlarge-window-horizontally)
   (define-key my-key-map (kbd "<f12>") 'sr-speedbar-toggle))
 
 (my-keybindings (current-global-map))
