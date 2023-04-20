55d899b113e7d494a11e73361dd7a26080da9020
Anton Borodavchenko <an2nb2@gmail.com>
2023-04-20 09:59:14 +0200

    fix python env

--- user-lisp/setup-elpy.el
+++ user-lisp/setup-elpy.el
@@ -1,9 +1,15 @@
 (require 'elpy)
+(require 'lsp-jedi)
+(require 'use-package)
 
 (elpy-enable)
 (add-hook 'python-mode-hook
           (lambda ()
             (pyvenv-workon "emacs")))
+
+;; https://github.com/jorgenschaefer/elpy/issues/1749
+(setenv "PYTHONIOENCODING" "utf-8")
+(add-to-list 'process-coding-system-alist '("python" . (utf-8 . utf-8)))
 
 ;; https://debbugs.gnu.org/cgi/bugreport.cgi?bug=25753#44
 (with-eval-after-load 'python
@@ -16,6 +22,15 @@
        (get-buffer-process (current-buffer))
        nil "_"))))
 
+(use-package lsp-jedi
+  :ensure t
+  :config
+  (with-eval-after-load "lsp-mode"
+    (add-to-list 'lsp-disabled-clients 'pyls)
+    (add-to-list 'lsp-enabled-clients 'jedi)))
+
 (setq elpy-rpc-backend "jedi")
+
+(define-key global-map (kbd "C-c C-j") 'elpy-goto-definition)
 
 (provide 'setup-elpy)
