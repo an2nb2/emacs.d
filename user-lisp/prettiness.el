eb7d45471ba4527bbc3e4964fdb9c5ab9004d17d
Anton Borodavchenko <an2nb2@gmail.com>
2023-04-20 10:00:10 +0200

    update theme

--- user-lisp/prettiness.el
+++ user-lisp/prettiness.el
@@ -1,22 +1,33 @@
 ;; prettiness.el
 ;; Visual settings and appearance tweaks
 
-;; (require 'monokai-theme)
-;; (load-theme 'adwaita)
-;; (require 'silkworm-theme)
-;; (require 'moe-dark-theme)
-;; (require 'leuven-theme)
-(require 'solarized-dark-theme)
+;; theme
+(load-theme 'material t)
+; (load-theme 'material-light t)
+
+; (require 'monokai-theme)
+; (load-theme 'adwaita)
+; (require 'silkworm-theme)
+; (require 'moe-dark-theme)
+; (require 'leuven-theme)
+; (require 'solarized-dark-theme)
+
+; (load-theme 'ample t t)
+; (load-theme 'ample-flat t t)
+; (load-theme 'ample-light t t)
+; (enable-theme 'ample)
+
+;; modeline
 (require 'spaceline-config)
 
 (spaceline-emacs-theme)
 
+;; Enable line numbers
 (global-linum-mode t)
 
-
+;; Font
 (set-face-attribute 'default nil :height 150)
 
-;; Set font
 (if window-system
     (if (member "DejaVu Sans Mono" (font-family-list))
         (set-face-attribute 'default nil :font "DejaVu Sans Mono" :height 150)))
